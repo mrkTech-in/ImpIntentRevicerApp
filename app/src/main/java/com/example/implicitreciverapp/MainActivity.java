bf9e9295ede100c265d2d1764b1cebe48daba2c1
MR.K <vpa.vishnupa@gamil.com>
2021-06-01 19:58:07 +0530

    ImplicitIntentReciverApp

---
+++ app/src/main/java/com/example/implicitreciverapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.implicitreciverapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        if(uri != null){
+            String  uri_string =  getString(R.string.uri) + uri.toString();
+            TextView textView = findViewById(R.id.uritext);
+            textView.setText(uri_string);
+
+
+        }
+
+    }
+
+}
